bcdd08dbea60229abf5ec98dfb470266d8201e75
Sean <scannon@protonmail.com>
2026-06-16 11:19:28 -0700

    added events and repos endpoints

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from db import SessionLocal
 from sqlalchemy.orm import Session
 from sqlalchemy import select, func
 from schemas import LanguageStat, EventOut, RepoOut
-from models import RepositoryLanguage
+from models import RepositoryLanguage, Event, Repository
 
 app = FastAPI()
 
@@ -21,3 +21,17 @@ def get_languages(session: Session = Depends(get_db)):
                 )
     rows = session.execute(stmt).all()
     return [LanguageStat(language=row.language, bytes=row.bytes) for row in rows]
+
+@app.get("/events", response_model=list[EventOut])
+def get_events(type: str | None = None, session: Session = Depends(get_db)):
+    stmt = select(Event.id, Event.type, Event.repo_name, Event.created_at).order_by(Event.created_at.desc())
+    if type is not None:
+        stmt = stmt.where(Event.type == type)
+    rows = session.execute(stmt).all()
+    return [EventOut(id=row.id, type=row.type, repo_name=row.repo_name, created_at = row.created_at) for row in rows]
+
+@app.get("/repos", response_model=list[RepoOut])
+def get_repos(session: Session = Depends(get_db)):
+    stmt = select(Repository.full_name, Repository.description, Repository.primary_language, Repository.pushed_at).order_by(Repository.pushed_at.desc())
+    rows = session.execute(stmt).all()
+    return [RepoOut(full_name=row.full_name, description=row.description, primary_language=row.primary_language, pushed_at=row.pushed_at) for row in rows]
